69aec1d483fd40274e059d77bf05f7cdba0b2adc
marksalvana <email@marksalvana.com>
2023-06-23 14:07:18 +0800

    particles.js config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,6 @@
 <script>
     import '../app.css';
-    import Particles from '../components/particles.svelte';
 
 </script>
 
-<main>
-
-    <Particles />
-    <slot />
-</main>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,28 @@
 <script>
-  </script>
-  
-<div style="min-width: 100%; min-height:100%;">
-test
-</div>
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    if (typeof window !== 'undefined') {
+      import('particles.js').then(({ default: particlesJS }) => {
+        particlesJS.load('particles-js', './particles.json', function() {
+          console.log('callback - particles.js config loaded');
+        });
+      });
+    }
+  });
+</script>
+
+<div id="particles-js"></div>
+
+<style>
+  #particles-js {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    background-color: #920000;
+    background-image: url("");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: 50% 50%;
+  }
+</style>
