2dcf7229de229cd410379aed377dcb3cf3e8f474
marksalvana <email@marksalvana.com>
2023-07-06 20:14:41 +0800

    profile updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,68 @@
 <script>
-  import { onMount } from 'svelte';
+	import Particles from 'svelte-particles';
+	import { loadFull } from 'tsparticles';
 
-  onMount(() => {
-    if (typeof window !== 'undefined') {
-      import('particles.js').then(({ default: particlesJS }) => {
-        particlesJS.load('particles-js', './particles.json', function() {
-          console.log('callback - particles.js config loaded');
-        });
-      });
-    }
-  });
+	let particlesConfig = {
+		particles: {
+			color: {
+				value: '#000'
+			},
+			links: {
+				enable: true,
+				color: '#000'
+			},
+			number: {
+				value: 80
+			},
+			move: {
+				enable: true
+			}
+		}
+	};
+
+	let particlesInit = async (engine) => {
+		// you can use main to customize the tsParticles instance adding presets or custom shapes
+		// this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+		// starting from v2 you can add only the features you need reducing the bundle size
+		await loadFull(engine);
+	};
 </script>
 
-<div id="particles-js"></div>
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<Particles id="tsparticles" class="foo bar" style="" options={particlesConfig} {particlesInit} />
+</section>
 
 <style>
-  #particles-js {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    background-color: #920000;
-    background-image: url("");
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: 50% 50%;
-  }
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		display: block;
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
 </style>
